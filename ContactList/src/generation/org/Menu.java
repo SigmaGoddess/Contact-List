37e7f3197511740529afd05f64e903785312c60c
Sheila Miranda <shei_xdxdxd@hotmail.com>
2025-09-29 14:39:11 -0600

    feat: se agregaron datos en menu test

--- ContactList/src/generation/org/Menu.java
+++ ContactList/src/generation/org/Menu.java
@@ -1,10 +1,35 @@
 package generation.org;
 
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 public class Menu {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	void desplegarMenu() {
+		System.out.println("Bienvenido al cajero automático");
+		System.out.println("Qué deseas hacer? Ingrese el número de opción");
+		System.out.println("1.- Añadir Contacto");
+		System.out.println("2.- Saber si un contacto existe");
+		System.out.println("3.- Mostrar Contactos");
+		System.out.println("4.- Buscar Contacto");
+		System.out.println("5.- Eliminar Contacto");
+		System.out.println("6.- Modificar Télefono");
+		System.out.println("7.- Saber cuántos espacios me quedan");
 
 	}
 
+	int registrarOpcion() {
+		Scanner sc = new Scanner(System.in);
+		return sc.nextInt();
+	}
+
+	int validarEntrada(int opcion) {
+		while (opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4 && opcion != 5 && opcion != 6 && opcion != 7) {
+			System.out.println("Error. ¡Opción inválida!");
+			desplegarMenu();
+			opcion = registrarOpcion();
+		}
+		return opcion;
+	}
 }
